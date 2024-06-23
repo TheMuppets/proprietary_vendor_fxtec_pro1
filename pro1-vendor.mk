38fae62a13aa4c54076deda84cc78cc1cfab2b83
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:41:54 +0100

    pro1: Add IMS libs symlinks
    Change-Id: Ifb70f144f82bcd130b0dd44e9d72da5ccd7f6bf0

--- pro1-vendor.mk
+++ pro1-vendor.mk
@@ -784,3 +784,7 @@ PRODUCT_PACKAGES += \
     dpmapi \
     qcrilhook \
     vendor.qti.gnss@4.0-service
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64
