12d78160f87f16138364b6ee49dd60c2e95df2ca
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-12 14:15:15 -0500

    fix com.qualcomm.qti.dpm.api@1.0 ELF checks
    Change-Id: I759752ccc133d7047d415eb83c5d3c55820437a4

--- pro1-vendor.mk
+++ pro1-vendor.mk
@@ -416,7 +416,7 @@ PRODUCT_PACKAGES += \
     libxapi_mfe \
     libasphere \
     com.qualcomm.qti.ant@1.0 \
-    com.qualcomm.qti.dpm.api@1.0 \
+    com.qualcomm.qti.dpm.api@1.0_vendor \
     com.qualcomm.qti.imscmservice@2.0 \
     com.qualcomm.qti.imscmservice@2.1 \
     com.qualcomm.qti.imscmservice@2.2 \
