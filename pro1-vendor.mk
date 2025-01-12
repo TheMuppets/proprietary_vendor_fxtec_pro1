61246ed0fa1e24ad92f8cfd22307a74e3aac21d8
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-12 15:02:30 -0500

    fix vendor.qti.hardware.fm@1.0 ELF checks
    Change-Id: I43f0824f88c24dd9d039f9075090c17f4266ab2d

--- pro1-vendor.mk
+++ pro1-vendor.mk
@@ -569,7 +569,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.data.latency@1.0 \
     vendor.qti.hardware.data.lce@1.0 \
     vendor.qti.hardware.data.qmi@1.0 \
-    vendor.qti.hardware.fm@1.0 \
+    vendor.qti.hardware.fm@1.0_vendor \
     vendor.qti.hardware.mwqemadapter@1.0 \
     vendor.qti.hardware.qccsyshal@1.0_vendor \
     vendor.qti.hardware.qccvndhal@1.0-halimpl \
