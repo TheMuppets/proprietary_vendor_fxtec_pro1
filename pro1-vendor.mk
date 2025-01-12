2a436173b75d81be944e93ff9603703f9c891bdd
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-12 13:08:05 -0500

    pro1: Remove unused libcneqmiutils
    It got added back by mistake
    
    Change-Id: I852e94325ac593bef49d4a4fe793ce6d22b5644e

--- pro1-vendor.mk
+++ pro1-vendor.mk
@@ -545,7 +545,6 @@ PRODUCT_COPY_FILES += \
     vendor/fxtec/pro1/proprietary/vendor/lib64/libcne.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcne.so \
     vendor/fxtec/pro1/proprietary/vendor/lib64/libcneapiclient.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcneapiclient.so \
     vendor/fxtec/pro1/proprietary/vendor/lib64/libcneoplookup.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcneoplookup.so \
-    vendor/fxtec/pro1/proprietary/vendor/lib64/libcneqmiutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcneqmiutils.so \
     vendor/fxtec/pro1/proprietary/vendor/lib64/libconfigdb.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libconfigdb.so \
     vendor/fxtec/pro1/proprietary/vendor/lib64/libdataitems.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdataitems.so \
     vendor/fxtec/pro1/proprietary/vendor/lib64/libdiag.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdiag.so \
